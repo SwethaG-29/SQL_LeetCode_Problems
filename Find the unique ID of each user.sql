34e1564b18166b910acdd80e478f5535840f2ea9
SwethaG-29 <swethaashu29@gmail.com>
2024-11-18 05:22:55 +0530

    Create Find the unique ID of each user.sql

---
+++ Find the unique ID of each user.sql
@@ -0,0 +1,24 @@
+/* Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+*/
+
+Create table If Not Exists Employees (id int, name varchar(20))
+Create table If Not Exists EmployeeUNI (id int, unique_id int)
+
+insert into Employees (id, name) values ('1', 'Alice')
+insert into Employees (id, name) values ('7', 'Bob')
+insert into Employees (id, name) values ('11', 'Meir')
+insert into Employees (id, name) values ('90', 'Winston')
+insert into Employees (id, name) values ('3', 'Jonathan')
+
+insert into EmployeeUNI (id, unique_id) values ('3', '1')
+insert into EmployeeUNI (id, unique_id) values ('11', '2')
+insert into EmployeeUNI (id, unique_id) values ('90', '3')
+
+SELECT unique_id,name FROM Employees EMP
+LEFT JOIN EmployeeUNI UNI
+ON EMP.id = UNI.id
